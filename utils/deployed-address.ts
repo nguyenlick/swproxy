bff349843d2f9223537abbcd8797f734a6e468ed
nguyenlick lik <nguyenthilik05@gmail.com>
2024-08-15 10:25:28 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x00410a3FdF07dbe61d6Fd038bc8F3E1af2672433'
+export const ProxyAdmin = '0x9c15d1d7CD35739bf10C6465f9cc5840E7CcAC6e'
+export const SWTRImplementation = '0x9071A8Bc46F19C8e7c0Ec82F49A77E73DB8eF8C6'
     
